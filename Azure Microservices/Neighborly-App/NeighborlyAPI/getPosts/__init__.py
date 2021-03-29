5c2e2d4f0acf5ecc919636237234c1260985668a
Andreas Grosser <agrossi@gmx.de>
2021-03-29 15:30:00 +0200

    update mongo db uri

--- Azure Microservices/Neighborly-App/NeighborlyAPI/getPosts/__init__.py
+++ Azure Microservices/Neighborly-App/NeighborlyAPI/getPosts/__init__.py
@@ -10,7 +10,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "mongodb://myneighborly-db:bn0IRqxiU54HKbxLYZvsXjOnMkclpaOzWAHeRKbagx4EPAD8BeSlhAYjtx04yN8EA9LuFOsZ4jIsPkzAXKfqtg==@myneighborly-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@myneighborly-db@"  # TODO: Update with appropriate MongoDB connection information
+        url = "MONGDB URL"
         client = pymongo.MongoClient(url)
         database = client['myneighborly-db']
         collection = database['posts']
