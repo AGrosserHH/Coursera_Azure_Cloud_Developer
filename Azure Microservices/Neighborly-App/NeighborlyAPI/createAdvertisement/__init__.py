0c8945d70bfcc356bdb7f7d8e11dcdbb1c8530ee
Andreas Grosser <agrossi@gmx.de>
2021-03-31 10:32:01 +0200

    update files

--- Azure Microservices/Neighborly-App/NeighborlyAPI/createAdvertisement/__init__.py
+++ Azure Microservices/Neighborly-App/NeighborlyAPI/createAdvertisement/__init__.py
@@ -7,7 +7,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if request:
         try:
-            url = "MONGDB URL"
+            url = "mongodb://myneighborly-db:qHasaaGDH02MojORbigmpEdrAMhIlSCa7fiMooVsm8KsHLjFR0o9QL1AqlS4GvSSkiVUubeWa2nJyCUW5wxUlQ==@myneighborly-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@myneighborly-db@"
             client = pymongo.MongoClient(url)
             database = client['myneighborly-db']
             collection = database['advertisements']
--- Azure Microservices/Neighborly-App/NeighborlyAPI/getAdvertisements/__init__.py
+++ Azure Microservices/Neighborly-App/NeighborlyAPI/getAdvertisements/__init__.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "MONGDB URL"
+        url = "mongodb://myneighborly-db:qHasaaGDH02MojORbigmpEdrAMhIlSCa7fiMooVsm8KsHLjFR0o9QL1AqlS4GvSSkiVUubeWa2nJyCUW5wxUlQ==@myneighborly-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@myneighborly-db@"
         client = pymongo.MongoClient(url)
         database = client['myneighborly-db']
         collection = database['advertisements']
